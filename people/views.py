9261b7c552cdcf22b634eeada76cbb2fe0fa8005
SashaKarpo <prmzwe@gmail.com>
2023-02-13 14:06:47 +0300

    adapted the serializer to my model updated views for serializer

--- people/views.py
+++ people/views.py
@@ -13,14 +13,17 @@ from django.forms import model_to_dict
 
 class PeopleAPIview(APIView):
     def get(self, request):
-        lst = People.objects.all().values()
-        return Response({'posts': list(lst)})
+        w = People.objects.all()
+        return Response({'posts': PeopleSerializer(w, many=True).data})
 
     def post(self, request):
+        serializer = PeopleSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
         post_new = People.objects.create(
             title=request.data['title'],
             content=request.data['content'],
             cat_id=request.data['cat_id']
         )
 
-        return Response({'post': model_to_dict(post_new)})
+        return Response({'post': PeopleSerializer(post_new).data})
