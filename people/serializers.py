e20f954f8bcab9385360fcf364c2bf74881791c7
SashaKarpo <prmzwe@gmail.com>
2023-02-16 16:01:50 +0300

    added access restriction for API using standard classes: IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
    created my own permission classes

--- people/serializers.py
+++ people/serializers.py
@@ -2,27 +2,9 @@ from rest_framework import serializers
 from .models import *
 
 
-# class PeopleModel:
-#    def __init__(self, title, content):
-#        self.title = title
-#        self.content = content
-
-
 class PeopleSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
     class Meta:
         model = People
         fields = "__all__"
-
-# def encode():
-#    model = PeopleModel('Angelina Jolie', 'Content: Angelina Jolie')
-#    model_sr = PeopleSerializer(model)
-#    json = JSONRenderer().render(model_sr.data)
-#    print(json)
-#
-#
-# def decode():
-#    stream = io.BytesIO(b'{"title":"Angelina Jolie","content":"Content: Angelina Jolie"}')
-#    data = JSONParser().parse(stream)
-#    serializer = PeopleSerializer(data=data)
-#    serializer.is_valid()
-#   print(serializer.validated_data)
--- people/views.py
+++ people/views.py
@@ -2,25 +2,30 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import *
 from .serializers import *
+from .permissions import *
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.forms import model_to_dict
 from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 
 
 class PeopleAPIList(generics.ListCreateAPIView):
     queryset = People.objects.all()
     serializer_class = PeopleSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly, )
 
 
 class PeopleAPIUpdate(generics.UpdateAPIView):
     queryset = People.objects.all()
     serializer_class = PeopleSerializer
+    permission_classes = (IsOwnerOrReadOnly, )
 
 
 class PeopleAPIDestroy(generics.RetrieveDestroyAPIView):
     queryset = People.objects.all()
     serializer_class = PeopleSerializer
+    permission_classes = (IsAdminOrReadOnly, )
 
 # class PeopleViewSet(viewsets.ModelViewSet):
 #    # queryset = People.objects.all()
